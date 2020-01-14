606e10f396c361825d01c16d584335864f5bf2c3
Franco Canedo <francanedo1@hotmail.com>
2020-01-14 19:04:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ def load_library(file_path)
   hash
 end
 
-def get_japanese_emoticon(file_path = './lib/emoticons.yml', emoticon)
+def get_japanese_emoticon(file_path, emoticon)
   new_hash = load_library(file_path)
   
   if new_hash[:get_emoticon].include?(emoticon)
